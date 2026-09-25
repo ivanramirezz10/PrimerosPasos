5d577943d3d2508c5863a78333fa5540748c2207
ivanramirezz10 <ivanrra07.rl@gmail.com>
2026-09-25 11:57:40 +0200

    24-sept

--- src/refuerzo1/Ejercicio_18.java
+++ src/refuerzo1/Ejercicio_18.java
@@ -2,14 +2,16 @@ package refuerzo1;
 
 import java.util.Scanner;
 
-public class Ejercicio_9 {
+public class Ejercicio_18 {
     public static void main(String[] args) {
         int num1;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Introduce un número entero");
         num1 = sc.nextInt();
 
-        if ((num1 % 2 ==0) && !(num1 % 3 ==0)){
-            System.out.println("El número " + num1 + "es multiplo de 2 y 3");
+        if (num1 % 2 != 0){
+            num1++;
+        }
     }
-}}
+}
